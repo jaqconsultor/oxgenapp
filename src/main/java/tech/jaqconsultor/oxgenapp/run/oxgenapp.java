b45b3d251f04fbd0b140d3b079cdc92f268a0ae5
Jesus Aquino <jaqconsultor@gmail.com>
2023-11-23 22:53:29 -0400

    uu

--- src/main/java/tech/jaqconsultor/oxgenapp/run/oxgenapp.java
+++ src/main/java/tech/jaqconsultor/oxgenapp/run/oxgenapp.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class oxgenapp {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("oxgenapp-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		//DBServer.start("oxgenapp-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("oxgenapp"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
 
